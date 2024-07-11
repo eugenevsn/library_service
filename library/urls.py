59e40869871438ca1954ca9cc3d3ef351904c65f
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-07-11 18:54:06 +0300

    add borrowings urls to library urls

--- library/urls.py
+++ library/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/library/", include("books.urls", namespace="books")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
     path("api/users/", include("users.urls", namespace="users")),
 ]
