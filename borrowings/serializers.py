f46ecc3c826bcb9019bb87fbe3cd6c5e4c75cbed
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-07-12 02:02:57 +0300

    add validation method to borrowing serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,10 +1,29 @@
 from rest_framework import serializers
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError
 
 from borrowings.models import Borrowing
 from books.serializers import BookSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs=attrs)
+        borrow_date = timezone.now().date()
+        if "borrow_date" in data.keys():
+            borrow_date = data["borrow_date"]
+        dates = (
+            data["expected_return_date"],
+            data["actual_return_date"]
+        )
+        for return_date in dates:
+            if return_date:
+                Borrowing.validate_dates(
+                    borrow_date,
+                    return_date,
+                    ValidationError,
+                )
+        return data
 
     class Meta:
         model = Borrowing
