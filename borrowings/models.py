d1590226755202822a80f300b1d912f718158828
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-07-12 01:49:44 +0300

    add validation methods to borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from rest_framework.exceptions import ValidationError
 
 from books.models import Book
 from library import settings
@@ -19,6 +20,46 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings"
     )
+
+    @staticmethod
+    def validate_dates(borrow_date, return_date, error_to_raise):
+        if borrow_date > return_date:
+            raise error_to_raise(
+                "Borrow date cannot be after expected "
+                f"{borrow_date} > {return_date}"
+            )
+
+    @staticmethod
+    def validate_inventory(book, error_to_raise):
+        if not book.inventory > 0:
+            raise error_to_raise(
+                "Inventory: 0, you can`t borrow this book"
+            )
+
+    def clean(self):
+        borrow_date = self.borrow_date
+        if not borrow_date:
+            borrow_date = timezone.now().date()
+        dates = (self.expected_return_date, self.actual_return_date)
+        for return_date in dates:
+            if return_date:
+                Borrowing.validate_dates(
+                    borrow_date,
+                    return_date,
+                    ValidationError,
+                )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self) -> str:
         return f"{self.book} : {self.borrow_date}"
