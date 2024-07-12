b35384ded168f227fb85c84160470ba2de47198d
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-07-12 03:33:37 +0300

    add _params_to_ints method

--- borrowings/views.py
+++ borrowings/views.py
@@ -24,5 +24,10 @@ class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
             return BorrowingCreateSerializer
         return self.serializer_class
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
