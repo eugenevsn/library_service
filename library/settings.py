1771599d989cfe64da2ae970c07a820bda425d42
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-07-12 22:33:10 +0300

    add spectacular to settings

--- library/settings.py
+++ library/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "books",
     "users",
     "borrowings",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -125,7 +126,14 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Library Service API",
+    "DESCRIPTION": "Borrowing books",
+    "VERSION": "1.0.0",
 }
 
 SIMPLE_JWT = {
