c52de6314820b1a9e6f8ac9e5830494c88837503
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-07-17 23:54:59 +0300

    add payment viewset

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import (
+    PaymentSerializer,
+    PaymentDetailSerializer,
+    PaymentListSerializer,
+)
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def get_permissions(self):
+        if self.action in ["retrieve", "list"]:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.action in ["retrieve"]:
+            return PaymentDetailSerializer
+        return PaymentListSerializer
